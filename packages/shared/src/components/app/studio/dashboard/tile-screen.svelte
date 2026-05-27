55063fcf77ed7f39938423bb603aa77851f35d6c
AristideBH <aristide.bruneau@gmail.com>
2026-05-27 22:56:53 +0200

    feat: simplify tile-screen component by removing unused imports and functions

--- packages/shared/src/components/app/studio/dashboard/tile-screen.svelte
+++ packages/shared/src/components/app/studio/dashboard/tile-screen.svelte
@@ -1,58 +1,30 @@
 <script lang="ts">
   import * as Card from '../../../ui/card/index.js';
-  import { Badge } from '../../../ui/badge/index.js';
   import { Button } from '../../../ui/button/index.js';
   import {
-    Usb,
-    Bluetooth,
-    RefreshCw,
-    LogOut,
-    Trash2,
     SlidersHorizontal,
   } from '@lucide/svelte';
-  import { deviceStatus } from '../../../../store/deviceStatus.svelte.js';
-  import { disconnect, serial } from '../../../../serial/index.svelte.js';
 
-  import {
-    loadConfig,
-    saveConfig,
-    configState,
-    factoryReset,
-  } from '../../../../store/config.svelte.js';
-
-  type DS = NonNullable<typeof deviceStatus.data>;
-  const data = $derived(deviceStatus.data as DS | null);
-
-  // ── Connexion ────────────────────────────────────────────────
-  const usbOn = $derived(data?.connection?.usb === true);
-  const bleOn = $derived(data?.connection?.ble === true);
-  const bleSlot = $derived(data?.connection?.ble_slot ?? 0);
-
-  async function handleFactoryReset() {
-    if (
-      !confirm(
-        'Remettre la config à zéro ? Toutes les modifications seront perdues.',
-      )
-    )
-      return;
-    await factoryReset();
-    await loadConfig();
+  async function handleScreenOption() {
+   //TODO
+    alert('TODO');
+    
   }
 </script>
 
 <Card.Root class="@container/card row-span-2 pb-0">
   <Card.Header class="h-fit ">
-    <Card.Description>Screen</Card.Description>
+    <Card.Description>Ecran</Card.Description>
     <Card.Action>
       <Button
-        variant="outline"
+        variant="ghost"
         class="gap-1.5"
         size="xs"
-        on:click={handleFactoryReset}
+        on:click={() => handleScreenOption() }
       >
         <SlidersHorizontal class="size-3" />
-      </Button></Card.Action
-    >
+      </Button>
+      </Card.Action>
   </Card.Header>
   <div
     class="flex flex-col items-baseline justify-center h-full gap-4 bg-muted/50 text-balance"
